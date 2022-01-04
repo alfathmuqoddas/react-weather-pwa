ad9f667ab83d13a0029e34a0a95e8597b9e63b94
alfathmuqoddas <alfath.muqoddas@gmail.com>
2022-01-04 10:05:14 +0700

    adding codes

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-# Weather Progressive Web Application
-![Weather Data](https://i.imgur.com/3csowzj.png)
+# React Weather Modern PWA
 
-### [Live Site](https://inspiring-bhaskara-d21f88.netlify.app)
-
-## Introduction
-This is a code repository for the corresponding video tutorial. 
-
-In this video, we will create a PWA. We're going to use React with service workers.
-
-By the end of this video, you will have a strong understanding of what PWA is and how you can build, deploy and publish it.
-
-Setup:
-- run ```npm i && npm start``` for both client and server side to start the development server
+`npm install && npm start`
+This app was build with modern progressive web app, where you can pin the app in home screen of your smartphone just like when you install an app from App Store.
--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
-import React, { useState } from 'react';
-
-import { fetchWeather } from './api/fetchWeather';
-import './App.css';
+import React, {useState} from 'react';
 
 const App = () => {
-    const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState({});
-    
-    const search = async (e) => {
-        if(e.key === 'Enter') {
-            const data = await fetchWeather(query);
-
-            setWeather(data);
-            setQuery('');
-        }
+  
+  const [weather, setWeather] = useState({});
+  const [query, setQuery] = useState('');
+  
+  const getWeather = async (e) => {
+    e.preventDefault();
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=4018b500122ee53a2b2b0ccc505a5ae4&units=metric`;
+    const res = await fetch(url);
+    const resJson = await res.json();
+    console.log(resJson);
+    console.log(resJson.main);
+    if (resJson) {
+      setWeather(resJson);
     }
-
-    return (
-        <div className="main-container">
-            <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
-            {weather.main && (
-                <div className="city">
-                    <h2 className="city-name">
-                        <span>{weather.name}</span>
-                        <sup>{weather.sys.country}</sup>
-                    </h2>
-                    <div className="city-temp">
-                        {Math.round(weather.main.temp)}
-                        <sup>&deg;C</sup>
-                    </div>
-                    <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                        <p>{weather.weather[0].description}</p>
-                    </div>
-                </div>
-            )}
-        </div>
-    );
+    setQuery('');
+  }
+  
+  return (
+  <>
+      <div style={{backgroundColor: '#8932a8'}}>
+      <div className="container py-3">
+    <h1 className="text-center text-light">React Weather PWA</h1>
+      <form className="input-group mx-auto" onSubmit={getWeather} style={{width:'500px'}}>
+              <input type="text" className="form-control" value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder='Search for a city' />
+              <input type="submit" className="btn btn-primary" value="🔍" />
+          </form>
+      </div>
+      </div>
+        {weather.list && (
+         <div className="container mt-5">
+            <div className="card mx-auto mb-2" style={{width:'18rem'}}>
+            <div class="card-header bg-primary text-light"><h4><bold>
+              {weather.city.name}, {weather.city.country}</bold></h4>
+            </div>
+            <img src={`https://openweathermap.org/img/wn/${weather.list[2].weather[0].icon}@4x.png`} class="card-img-top" alt="..." />
+            <div className="card-body bg-light">
+              <h1 className="card-text text-center">{weather.list[2].main.temp.toFixed(0)}°C</h1>
+              <h4 className="card-text text-center">{weather.list[2].weather[0].description}</h4>
+            </div>
+          </div>
+          <div className="card mx-auto mb-5" style={{width: '18rem'}}>
+             <ul class="list-group list-group-flush">
+              <li class="list-group-item">Wind: {weather.list[2].wind.speed} m/s | {weather.list[2].wind.deg}°</li>
+              <li class="list-group-item">Humidity: {weather.list[2].main.humidity}%</li>
+              <li class="list-group-item">Visibility: {weather.list[2].visibility*0.001} km</li>
+            </ul>
+          </div>
+         </div>
+        )}
+  </>
+  );
 }
 
 export default App;
