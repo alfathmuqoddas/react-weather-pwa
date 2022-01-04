f417e93c998a699e209c7456f9f006f90aab42cd
alfathmuqoddas <alfath.muqoddas@gmail.com>
2022-01-04 10:49:06 +0700

    Updates

---
+++ precache-manifest.d235287464652b196490870b36f7470e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b70b75c16ea48dc4bed0b98e04b831",
+    "url": "/alfathmuqoddas/react-weather-pwa/index.html"
+  },
+  {
+    "revision": "f40f3990aa95f8be0c75",
+    "url": "/alfathmuqoddas/react-weather-pwa/static/js/2.e1632085.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/alfathmuqoddas/react-weather-pwa/static/js/2.e1632085.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fec04f5056d705646cd",
+    "url": "/alfathmuqoddas/react-weather-pwa/static/js/main.eeffcb81.chunk.js"
+  },
+  {
+    "revision": "e1a06b474303bcf718f7",
+    "url": "/alfathmuqoddas/react-weather-pwa/static/js/runtime-main.5d78617d.js"
+  }
+]);
